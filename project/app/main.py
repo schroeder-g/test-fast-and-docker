e6ad66444d5b4a53f550429eb029a3fedba71757
Alex Goncalves <schroedergoncalves@gmail.com>
2021-10-01 23:45:55 -0400

    setup for building the application. Following along with the testdriven io tutorial.

---
+++ project/app/main.py
@@ -0,0 +1,15 @@
+# project/app/main.py
+
+from app.config import get_settings, Settings
+from fastapi import FastAPI, Depends
+
+
+app = FastAPI()
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
