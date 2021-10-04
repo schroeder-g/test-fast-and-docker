4f949cfa69f932907c55694d9f8a8cbc230c6232
Alex Goncalves <schroedergoncalves@gmail.com>
2021-10-03 23:54:44 -0400

    Added Aerich ORM and Pytest scaffolding

---
+++ project/app/models/tortoise.py
@@ -0,0 +1,12 @@
+# Tortoise provides an ORM for simplifying our db interactions
+
+from tortoise import fields, models
+
+
+class TextSummary(models.Model):
+    url = fields.TextField()
+    summary = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
---
+++ project/tests/test_ping.py
@@ -0,0 +1,9 @@
+def test_ping(test_app):
+    # Given the state in test_app
+
+    # When
+    response = test_app.get("/ping")
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
