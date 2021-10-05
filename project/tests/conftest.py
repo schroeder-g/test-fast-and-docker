0f9d5c5106db504ef11a3d1b2ce6646a9bac88dd
Alex Goncalves <schroedergoncalves@gmail.com>
2021-10-05 11:04:59 -0400

    added passing test for new post route

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -5,6 +5,7 @@ from fastapi.testclient import TestClient
 
 from app.main import create_application
 from app.config import get_settings, Settings
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override():
@@ -21,3 +22,17 @@ def test_app():
         yield test_client
 
     # tear down
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
+        yield test_client
