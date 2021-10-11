1bce37601d8b0fe0650a89a109e1c82fc63159f5
Alex Goncalves <schroedergoncalves@gmail.com>
2021-10-11 19:45:29 -0400

    correcting inline comment prettification error to satisfy Flake8

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.tortoise import SummarySchema
-from app.models.pydantic import ( # isort:skip
+from app.models.pydantic import (  # isort:skip
     SummaryPayloadSchema,
     SummaryResponseSchema,
     SummaryUpdatePayloadSchema,
